f61fe154ba7b13d9fdafcb9187d9723b07fe0e22
Archieeeeee <longkas@gmail.com>
2019-09-26 17:57:52 +0800

    Update install-bbr-centos.sh

--- install-bbr-centos.sh
+++ install-bbr-centos.sh
@@ -119,7 +119,7 @@ select_grub_auto_reboot() {
         fi
         echo "============================="
         
-        for (i=1; i<15; i++) do
+        for ((i=1; i<15; i++)) do
         	local bootKnel = `grubby --info=$i | egrep '^kernel' | sed -r "s/.*vmlinuz-(.*)/\1/" | cut -d '-' -f 1`
         	# stop loop if boot menu overflow: grubby: kernel not found
         	if [[ $bootKnel == grubby* ]]; then
