e4605951f5f9a08f6936341d563f290fb3a8c02c
Archieeeeee <longkas@gmail.com>
2024-06-06 21:10:39 +0800

    Create post-install-node.sh

---
+++ post-install-node.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+systemctl stop kdump.service; systemctl disable kdump.service;
+
+ckk=$(cat /etc/default/grub |grep crashkernel)
+if [ "$(echo "$ckk" | wc -l)" -lt 1 ]; then
+  echo "no crashkernel"
+  exit 0
+else
+  echo "grub cmd is $ckk"
+fi
+
+grubby --update-kernel=ALL --args="crashkernel=no"
+grubby --remove-args="crashkernel=no" --update=ALL
+
+sleep 3
+reboot
