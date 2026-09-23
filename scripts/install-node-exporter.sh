6fbbc846da61a078f5a0d9a6b3e2d0017ce06998
Archieeeeee <longkas@gmail.com>
2026-09-23 22:38:11 +0800

    Update install-node-exporter.sh

--- scripts/install-node-exporter.sh
+++ scripts/install-node-exporter.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#pkill node_exporter; curl -s https://raw.githubusercontent.com/Archieeeeee/vpn/refs/heads/master/scripts/install-node-exporter.sh | bash
+
+
 dnf copr enable ibotty/prometheus-exporters -y
 dnf install node_exporter -y
 #for epel 9
