1e75ab06f60564e9c86769d3f768fc5d7e60ecd9
Archieeeeee <longkas@gmail.com>
2026-09-23 22:44:50 +0800

    Update enable.root.login.sh

--- scripts/enable.root.login.sh
+++ scripts/enable.root.login.sh
@@ -3,9 +3,11 @@
 #curl -s https://raw.githubusercontent.com/Archieeeeee/vpn/refs/heads/master/scripts/enable.root.login.sh | bash 
 
 #allow password
-sed -i 's/^#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/*.conf 2>/dev/null
 
 #allow root user
-sed -i 's/^#\?PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^#\?PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^#\?PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_config.d/*.conf 2>/dev/null
 
 sudo systemctl restart sshd
